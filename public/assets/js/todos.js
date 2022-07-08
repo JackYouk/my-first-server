5fccb1bfb14d74a217de68e0313d94e9e16183e4
JackYouk <jackyoukstetter1@gmail.com>
2022-07-08 21:14:50 +0100

    todo list with input that goes to psuedodatabase (json file) and then updates the ui

--- public/assets/js/todos.js
+++ public/assets/js/todos.js
@@ -1,4 +1,43 @@
+
+const todosList = document.querySelector('.todos');
+const todoInput = document.querySelector('#todoInput');
+const submitBtn = document.querySelector('#submitTodo');
+
+
+submitBtn.addEventListener('click', function(){
+    const ntodo = {
+        todo: todoInput.value,
+    }
+
+    fetch('/api/todos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ntodo)
+    })
+        .then(res => res.json())
+        .then(newTodo => {
+            todoInput.value = '';
+            const newTodoLi = document.createElement('li');
+            newTodoLi.textContent = newTodo.todo;
+            todosList.appendChild(newTodoLi);
+        })
+})
+
 // GET localhost:3001/api/todos
 fetch('/api/todos')
     .then(res => res.json())
-    .then(todos => console.log(todos))
+    .then(todos => renderTodos(todos));
+
+
+const renderTodos = (todos) => {
+    todosList.innerHTML = '';
+
+    todos.forEach(todo => {
+        console.log(todo);
+        const todoItem = document.createElement('li');
+        todoItem.textContent = todo.todo;
+        todosList.appendChild(todoItem);
+    });
+}
